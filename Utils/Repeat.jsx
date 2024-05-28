69075f2fa0dc6f15bc0681de0acd94ac6bc963ca
MEr1992 <m.ebadi.r@gmail.com>
2024-05-28 11:43:48 +0330

    update Repeat of AiBi 1403/03/08 11:43

--- Utils/Repeat.jsx
+++ Utils/Repeat.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 import { FeatherIcon} from "@/Theme/Midone/Utils/FeatherIcon";
 import { useLang } from '@/lib';
 
-export const Repeat = ({child, parent, needles,className , count_data=1,classNameComp})=>{
+export const Repeat = ({child, parent, needles, className , count_data=1, classNameComp, other, displayClose = true, displayPlus=true})=>{
     let[state, setState] = useState({
         count: 1,
         deleted: [],
@@ -29,23 +29,31 @@ export const Repeat = ({child, parent, needles,className , count_data=1,classNam
 
     let Comp = child;
 
-    return <div className={'items-center col-span-12'+ (className?className:"")}>
+    return <div className={'items-center col-span-12 '+ (className?className:"")}>
         {
             Array(state.count).fill(0).map((i, key)=>{
                 if(state.deleted.indexOf(key) == -1)
                     return <div className='grid grid-cols-12 gap-2' key={key}>
                                 <div className='col-span-11 '>
                                     <div className='grid grid-cols-12 gap-2'>
-                                        <Comp index={key} key={key} parent={parent} needles={needles} className={"col-span-11 "+classNameComp} />
+                                        <Comp index={key} key={key+other?.toString()} parent={parent} needles={needles} className={"col-span-11 "+classNameComp} {...other} />
                                     </div>
                                 </div>
-                                <div className='col-span-1 text-center pt-6 mt-3'>
-                                    <FeatherIcon spanWrapperclassName="icon-plus" iconClassName="inline-block" name="XOctagon" color="darkred"  onClick={()=>remove(key)} />
-                                </div>
+                                {(displayClose)?
+                                    <div className="col-span-1 text-center pt-6 mt-3">
+                                        <FeatherIcon spanWrapperClass="icon-plus" iconClassName="mt-5" name="XOctagon" color="darkred"  onClick={()=>remove(key)} />
+                                    </div>
+                                :
+                                    ""
+                                }
                             </div>
                 }
             )
         }
-        <FeatherIcon iconClassName="mt-1" name="PlusSquare"  onClick={add} />
+        {(displayPlus)?
+            <FeatherIcon iconClassName="mt-2" name="PlusSquare"  onClick={add} />
+        :
+            ""
+        }
     </div>
 }
